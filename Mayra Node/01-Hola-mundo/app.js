4abc5fc116e3c214f285188bd2062571664be123
mayra manzaneda <urielortega300@gmail.com>
2023-09-25 22:11:00 -0300

    semestre 4
    en proseso

--- Mayra Node/01-Hola-mundo/app.js
+++
@@ -1,7 +0,0 @@
-const nombre = 'Ariel';
-console.log(nombre);
-
-const nombre2 = 'juan';
-console.log(nombre);
-
-console.log('Hola mun do desde Node');
--- Mayra Node/01-Hola-mundo/app2.js
+++
@@ -1,5 +0,0 @@
-const saludar = () => {
-  //funcion de flecha
-  return " saludar ${ nombre } "; //usamos el back tic
-}; // esto es igual a: 'saludos' + nombre;
-console.log(saludar("ariel"));
