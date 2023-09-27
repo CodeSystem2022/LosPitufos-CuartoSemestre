65e16717a2287225f0f3d9ad8170c060f790575e
mayra manzaneda <urielortega300@gmail.com>
2023-09-26 23:26:31 -0300

    Create f_string.py
    .

---
+++ Mayra Python/f_string.py
@@ -0,0 +1,7 @@
+nombre = 'Pepe'
+edad = 28
+sueldo =3000
+mensaje =f' Nombre{nombre} edad {edad} sueldo {sueldo:.2f}' #interpolacion
+
+print(mensaje) #da ciertos formatos
+print(nombre, edad , sueldo , sep=',') #enviamos varias variables enviamos tupla
